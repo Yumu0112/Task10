0bb0a4f8e74947632e81d0e4d891d889e957cbaa
Yumu0112 <yumu0112@icloud.com>
2023-06-28 13:47:40 +0900

    error status

--- src/main/java/com/example/customerdb/exception/NotFoundException.java
+++ src/main/java/com/example/customerdb/exception/NotFoundException.java
@@ -3,6 +3,6 @@ package com.example.customerdb.exception;
 public class NotFoundException extends RuntimeException{
 
     public NotFoundException(int id) {
-        super("ID:" + id + "was not found");
+        super("ID:" + id + " was not found");
     }
 }
