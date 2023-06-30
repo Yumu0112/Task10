14f341515597c5760761f9079a5e0a8f194a03d4
Yumu0112 <yumu0112@icloud.com>
2023-06-30 13:17:13 +0900

    add response message

--- src/main/java/com/example/customerdb/controller/PurchaseInfoController.java
+++ src/main/java/com/example/customerdb/controller/PurchaseInfoController.java
@@ -2,10 +2,12 @@ package com.example.customerdb.controller;
 
 import com.example.customerdb.entity.PurchaseInfo;
 import com.example.customerdb.service.PurchaseInfoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -31,25 +33,29 @@ public class PurchaseInfoController {
     }
 
     @PostMapping("/purchase-info")
-    public void addInfo(@RequestBody PurchaseInfo purchaseInfo) {
+    public ResponseEntity<Map<String,String>> addInfo(@RequestBody PurchaseInfo purchaseInfo) {
         purchaseInfoService.addInfo(purchaseInfo);
+        return ResponseEntity.ok(Map.of("status", "character successfully created"));
     }
 
 
     @PutMapping("/purchase-info/{id}")
-    public void updateInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo) {
+    public ResponseEntity<Map<String,String>> updateInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo) {
         purchaseInfoService.updateInfo(id, purchaseInfo);
+        return ResponseEntity.ok(Map.of("status", "character successfully updated"));
+
     }
 
     @PatchMapping("/purchase-info/{id}")
-    public void editInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo)  {
+    public ResponseEntity<Map<String,String>> editInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo)  {
         purchaseInfoService.editInfo(id, purchaseInfo);
-}
+        return ResponseEntity.ok(Map.of("status", "Info successfully updated"));
+    }
 
     @DeleteMapping("/purchase-info/{id}")
-    public ResponseEntity<String> deleteInfo(@PathVariable int id) {
+    public ResponseEntity<Map<String, String>> deleteInfo(@PathVariable int id) {
         purchaseInfoService.deleteInfo(id);
-        return ResponseEntity.ok("Purchase info with ID " + id + " has been deleted");
+        return ResponseEntity.ok(Map.of("status", "Info successfully deleted"));
     }
 
 }
