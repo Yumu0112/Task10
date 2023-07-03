e50cd280f4de9d94842e6c42b92284a30ec24759
Yumu0112 <118417799+Yumu0112@users.noreply.github.com>
2023-07-03 14:12:15 +0900

    Revert "Serviceクラス単体テスト"

--- src/main/java/com/example/customerdb/entity/PurchaseInfo.java
+++ src/main/java/com/example/customerdb/entity/PurchaseInfo.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 
 @Getter
@@ -22,20 +21,5 @@ public class PurchaseInfo {
         this.email = email;
         this.purchaseDate = purchaseDate;
         this.price = price;
-    }
-
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PurchaseInfo that = (PurchaseInfo) o;
-        return id == that.id && price == that.price && Objects.equals(name, that.name) && Objects.equals(email, that.email) && Objects.equals(purchaseDate, that.purchaseDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, purchaseDate, price);
     }
 }
