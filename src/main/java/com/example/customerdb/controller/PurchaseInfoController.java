b233623af763fbe6cb16b6ab1bee606b15d9fe4e
Yumu0112 <yumu0112@icloud.com>
2023-07-03 13:23:21 +0900

    レスポンスクラスの作成と修正

--- src/main/java/com/example/customerdb/controller/PurchaseInfoController.java
+++ src/main/java/com/example/customerdb/controller/PurchaseInfoController.java
@@ -2,9 +2,15 @@ package com.example.customerdb.controller;
 
 import com.example.customerdb.entity.PurchaseInfo;
 import com.example.customerdb.service.PurchaseInfoService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,9 +21,6 @@ import java.util.Map;
 public class PurchaseInfoController {
 
     private final PurchaseInfoService purchaseInfoService;
-    private final HttpStatus httpStatus = HttpStatus.OK;
-
-    private final Map<String, Object> response = new HashMap<>();
 
     private final Map<String, Map<String, Object>> responseBody = new HashMap<>();
     public PurchaseInfoController(PurchaseInfoService purchaseInfoService) {
@@ -38,41 +41,41 @@ public class PurchaseInfoController {
     }
 
     @PostMapping("/purchase-info")
-    public ResponseEntity<Map<String, Map<String, Object>>> addInfo(@RequestBody PurchaseInfo purchaseInfo) {
+    public PurchaseInfoJsonResponse addInfo(@RequestBody PurchaseInfo purchaseInfo) {
+        Map<String, Object> response = new HashMap<>();
         purchaseInfoService.addInfo(purchaseInfo);
 
         response.put("content", purchaseInfo);
-        responseBody.put(httpStatus.toString(), response);
 
-        return ResponseEntity.ok(responseBody);
+        return new PurchaseInfoJsonResponse(purchaseInfo);
     }
 
 
     @PutMapping("/purchase-info/{id}")
-    public ResponseEntity<Map<String, Map<String, Object>>> updateInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo) {
+    public PurchaseInfoJsonResponse updateInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo) {
+        Map<String, Object> response = new HashMap<>();
         purchaseInfoService.updateInfo(id, purchaseInfo);
 
         response.put("content", purchaseInfo);
-        responseBody.put(httpStatus.toString(), response);
 
-        return ResponseEntity.ok(responseBody);
+        return new PurchaseInfoJsonResponse(purchaseInfo);
     }
 
     @PatchMapping("/purchase-info/{id}")
-    public ResponseEntity<Map<String, Map<String, Object>>> editInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo)  {
+    public PurchaseInfoJsonResponse editInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo)  {
+        Map<String, Object> response = new HashMap<>();
         purchaseInfoService.editInfo(id, purchaseInfo);
 
         response.put("content", purchaseInfo);
-        responseBody.put(httpStatus.toString(), response);
 
-        return ResponseEntity.ok(responseBody);
+        return new PurchaseInfoJsonResponse(purchaseInfo);
     }
 
     @DeleteMapping("/purchase-info/{id}")
     public ResponseEntity<Map<String, Object>> deleteInfo(@PathVariable int id) {
+        Map<String, Object> response = new HashMap<>();
         purchaseInfoService.deleteInfo(id);
 
-        response.put("status", httpStatus.toString());
         response.put("message", "Info successfully deleted");
 
         return ResponseEntity.ok(response);
---
+++ src/main/java/com/example/customerdb/controller/PurchaseInfoJsonResponse.java
@@ -0,0 +1,16 @@
+package com.example.customerdb.controller;
+
+import com.example.customerdb.entity.PurchaseInfo;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PurchaseInfoJsonResponse {
+    private PurchaseInfo content;
+
+}
