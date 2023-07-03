d809ccdfbf80026071b7f9a752d7cec547a4fb03
Yumu0112 <yumu0112@icloud.com>
2023-07-03 13:30:31 +0900

    コンフリクト解消

--- src/main/java/com/example/customerdb/controller/PurchaseInfoController.java
+++ src/main/java/com/example/customerdb/controller/PurchaseInfoController.java
@@ -2,82 +2,61 @@ package com.example.customerdb.controller;
 
 import com.example.customerdb.entity.PurchaseInfo;
 import com.example.customerdb.service.PurchaseInfoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 
 @RestController
 public class PurchaseInfoController {
 
     private final PurchaseInfoService purchaseInfoService;
 
-    private final Map<String, Map<String, Object>> responseBody = new HashMap<>();
     public PurchaseInfoController(PurchaseInfoService purchaseInfoService) {
         this.purchaseInfoService = purchaseInfoService;
     }
+
     @GetMapping("/purchase-info")
-    public List<PurchaseInfo> info() {
+    public List<PurchaseInfo> getInfo() {
         return purchaseInfoService.findAll();
     }
 
-    //    Serviceを使ったpriceのみの一覧表示
     @GetMapping("/pricelist")
-    public List<PurchaseInfoResponse> price() {
+    public List<PurchaseInfoResponse> getPriceList() {
         List<PurchaseInfo> purchaseInfos = purchaseInfoService.findAll();
         List<PurchaseInfoResponse> purchaseInfoResponses = purchaseInfos.stream().map(PurchaseInfoResponse::new).toList();
-
         return purchaseInfoResponses;
     }
 
     @PostMapping("/purchase-info")
-    public PurchaseInfoJsonResponse addInfo(@RequestBody PurchaseInfo purchaseInfo) {
-        Map<String, Object> response = new HashMap<>();
+    public ResponseEntity<PurchaseInfoJsonResponse> addInfo(@RequestBody PurchaseInfo purchaseInfo) {
         purchaseInfoService.addInfo(purchaseInfo);
-
-        response.put("content", purchaseInfo);
-
-        return new PurchaseInfoJsonResponse(purchaseInfo);
+        PurchaseInfoJsonResponse response = new PurchaseInfoJsonResponse(purchaseInfo);
+        return ResponseEntity.ok(response);
     }
 
-
     @PutMapping("/purchase-info/{id}")
-    public PurchaseInfoJsonResponse updateInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo) {
-        Map<String, Object> response = new HashMap<>();
+    public ResponseEntity<PurchaseInfoJsonResponse> updateInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo) {
         purchaseInfoService.updateInfo(id, purchaseInfo);
-
-        response.put("content", purchaseInfo);
-
-        return new PurchaseInfoJsonResponse(purchaseInfo);
+        PurchaseInfoJsonResponse response = new PurchaseInfoJsonResponse(purchaseInfo);
+        return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/purchase-info/{id}")
-    public PurchaseInfoJsonResponse editInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo)  {
-        Map<String, Object> response = new HashMap<>();
+    public ResponseEntity<PurchaseInfoJsonResponse> editInfo(@PathVariable int id, @RequestBody PurchaseInfo purchaseInfo) {
         purchaseInfoService.editInfo(id, purchaseInfo);
-
-        response.put("content", purchaseInfo);
-
-        return new PurchaseInfoJsonResponse(purchaseInfo);
+        PurchaseInfoJsonResponse response = new PurchaseInfoJsonResponse(purchaseInfo);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/purchase-info/{id}")
     public ResponseEntity<Map<String, Object>> deleteInfo(@PathVariable int id) {
-        Map<String, Object> response = new HashMap<>();
         purchaseInfoService.deleteInfo(id);
-
+        Map<String, Object> response = new HashMap<>();
         response.put("message", "Info successfully deleted");
-
         return ResponseEntity.ok(response);
     }
 }
