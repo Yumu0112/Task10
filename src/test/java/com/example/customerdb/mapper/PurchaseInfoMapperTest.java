bdfb191989307d4fac7f19165a38434c68c57c01
Yumu0112 <yumu0112@icloud.com>
2023-07-04 15:41:51 +0900

    mapper test setting

--- src/test/java/com/example/customerdb/mapper/PurchaseInfoMapperTest.java
+++ src/test/java/com/example/customerdb/mapper/PurchaseInfoMapperTest.java
@@ -1,12 +1,17 @@
 package com.example.customerdb.mapper;
 
+import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import com.github.database.rider.spring.api.DBRider;
 
-//@DBRider
+@DBRider
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class PurchaseInfoMapperTest {
 
+    @Autowired
+    PurchaseInfoMapper purchaseInfoMapper;
 
 }
