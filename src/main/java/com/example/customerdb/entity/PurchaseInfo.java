0ab5604c69fb22c2fe0847ebc0c3a7bcbbbd552c
Yumu0112 <yumu0112@icloud.com>
2023-07-07 13:02:12 +0900

    change dependency

--- src/main/java/com/example/customerdb/entity/PurchaseInfo.java
+++ src/main/java/com/example/customerdb/entity/PurchaseInfo.java
@@ -3,7 +3,7 @@ package com.example.customerdb.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
